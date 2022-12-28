7238dcd80f2b264be1416643af9bb56e4b3c93ad
Tobias Wagner <Tobawa2601@gmail.com>
2022-12-28 22:07:58 +0100

    foosball-frontend-5 Creating a page that lists all players
    Corrected the players.service.ts stubs map

--- src/app/players/players.service.ts
+++ src/app/players/players.service.ts
@@ -3,51 +3,50 @@ import { Player, Players } from "./player/player.model";
 import { BehaviorSubject } from "rxjs";
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class PlayersService {
-  playersChange$ = new BehaviorSubject<Player[]>([]);
+    playersChange$ = new BehaviorSubject<Player[]>([]);
+    private playersStub: Players = [
+        { id: 0, firstName: 'Tobias', lastName: 'Wagner', email: 'Tobias.Wagner@MetallRente-Services.de' },
+        { id: 1, firstName: 'Gareth', lastName: 'Schmutz' },
+        { id: 2, firstName: 'Sven', lastName: 'Hanl' }
+    ];
+    private players: Map<Player['id'], Player> = new Map([]);
 
-  private players: Map<Player['id'], Player> = new Map(this.playersStub.map(player => [player.id, player]))
-  private playersStub: Players = [
-    { id: 0, firstName: 'Tobias', lastName: 'Wagner', email: 'Tobias.Wagner@MetallRente-Services.de' },
-    { id: 1, firstName: 'Gareth', lastName: 'Schmutz' },
-    { id: 2, firstName: 'Sven', lastName: 'Hanl' }
-  ];
+    constructor() {
+        this.setPlayers();
+    }
 
-  constructor() {
-    this.setPlayers();
-  }
+    public editPlayer(id: Player['id'], player: Player) {
+        this.players.set(id, player)
+        this.updatePlayers();
+    }
 
-  public editPlayer(id: Player['id'], player: Player) {
-    this.players.set(id, player)
-    this.updatePlayers();
-  }
+    public playerTrackBy(index: number, player: Player) {
+        return player.id;
+    }
 
-  public playerTrackBy(index: number, player: Player) {
-    return player.id;
-  }
+    public getPlayer(id: Player['id']) {
+        return this.players.get(id);
+    }
 
-  public getPlayer(id: Player['id']) {
-    return this.players.get(id);
-  }
+    public deletePlayer(id: Player['id']) {
+        this.players.delete(id);
+        this.updatePlayers();
+    }
 
-  public deletePlayer(id: Player['id']) {
-    this.players.delete(id);
-    this.updatePlayers();
-  }
+    public addPlayer(player: Player) {
+        this.players.set(player.id, player);
+        this.updatePlayers();
+    }
 
-  public addPlayer(player: Player) {
-    this.players.set(player.id, player);
-    this.updatePlayers();
-  }
+    private setPlayers() {
+        this.playersStub.forEach(player => this.players.set(player.id, player))
+        this.updatePlayers();
+    }
 
-  private setPlayers() {
-    this.playersStub.forEach(player => this.players.set(player.id, player))
-    this.updatePlayers();
-  }
-
-  private updatePlayers() {
-    this.playersChange$.next([...this.players.values()]);
-  }
+    private updatePlayers() {
+        this.playersChange$.next([...this.players.values()]);
+    }
 }
