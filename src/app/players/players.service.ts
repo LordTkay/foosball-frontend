a4b99af816b3921469fe7842a22c4fc28379a967
Tobias Wagner <Tobawa2601@gmail.com>
2022-12-28 22:05:40 +0100

    foosball-frontend-5 Creating a page that lists all players
    Changed how the player stubs are inserted, to be closer to the real implementation.

--- src/app/players/players.service.ts
+++ src/app/players/players.service.ts
@@ -6,16 +6,23 @@ import { BehaviorSubject } from "rxjs";
   providedIn: 'root'
 })
 export class PlayersService {
-  private playersStub: Players = [
-    {id: 0, firstName: 'Tobias', lastName: 'Wagner', email: 'Tobias.Wagner@MetallRente-Services.de'},
-    {id: 1, firstName: 'Gareth', lastName: 'Schmutz'},
-    {id: 2, firstName: 'Sven', lastName: 'Hanl'}
-  ];
+  playersChange$ = new BehaviorSubject<Player[]>([]);
 
   private players: Map<Player['id'], Player> = new Map(this.playersStub.map(player => [player.id, player]))
+  private playersStub: Players = [
+    { id: 0, firstName: 'Tobias', lastName: 'Wagner', email: 'Tobias.Wagner@MetallRente-Services.de' },
+    { id: 1, firstName: 'Gareth', lastName: 'Schmutz' },
+    { id: 2, firstName: 'Sven', lastName: 'Hanl' }
+  ];
 
-  playerChangeSubject = new BehaviorSubject([...this.players.values()]);
+  constructor() {
+    this.setPlayers();
+  }
 
+  public editPlayer(id: Player['id'], player: Player) {
+    this.players.set(id, player)
+    this.updatePlayers();
+  }
 
   public playerTrackBy(index: number, player: Player) {
     return player.id;
@@ -35,12 +42,12 @@ export class PlayersService {
     this.updatePlayers();
   }
 
-  public editPlayer(player: Player) {
-    this.players.set(player.id, player)
+  private setPlayers() {
+    this.playersStub.forEach(player => this.players.set(player.id, player))
     this.updatePlayers();
   }
 
   private updatePlayers() {
-    this.playerChangeSubject.next([...this.players.values()]);
+    this.playersChange$.next([...this.players.values()]);
   }
 }
