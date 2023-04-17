aa063dae52f9ff8bff723d9cf3c4fa22b3f465eb
Tobias Wagner <Tobawa2601@gmail.com>
2023-04-17 21:39:17 +0200

    Reset of players- and player-component

--- src/app/players/players.component.ts
+++ src/app/players/players.component.ts
@@ -1,37 +1,11 @@
-import { Component, OnDestroy, ViewChild, ViewContainerRef } from '@angular/core';
-import { PlayersService } from "./players.service";
-import { PlayerComponent } from "./player/player.component";
-import { Subscription, take } from "rxjs";
+import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-players',
     templateUrl: './players.component.html',
     styleUrls: ['./players.component.scss']
 })
-export class PlayersComponent implements OnDestroy {
+export class PlayersComponent {
 
-    @ViewChild('playersList', { read: ViewContainerRef }) playersList!: ViewContainerRef;
 
-    private subscriptions = new Subscription();
-
-    constructor(protected playersService: PlayersService) {
-    }
-
-    ngOnDestroy() {
-        this.subscriptions.unsubscribe();
-    }
-
-    /**
-     * Appends a player components to the list. It listens to the destroy event, to destroy itself if needed.
-     */
-    onAddPlayer() {
-        const newPlayerComponentRef = this.playersList.createComponent(PlayerComponent)
-        const destroySubscription = newPlayerComponentRef.instance
-            .destroy
-            .pipe(take(1))
-            .subscribe(() => {
-                newPlayerComponentRef.destroy();
-            });
-        this.subscriptions.add(destroySubscription);
-    }
 }
