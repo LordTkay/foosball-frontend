f5dff40507316363b911cc81850b4070a702ead7
Tobias Wagner <Tobawa2601@gmail.com>
2023-05-25 22:56:26 +0200

    Added the functionality to use escape, to cancel the current edit of a player

--- src/app/players/player/player.component.ts
+++ src/app/players/player/player.component.ts
@@ -5,68 +5,74 @@ import { deepClone } from "../../utility/deepCopy.function";
 import { NgForm } from "@angular/forms";
 
 @Component({
-    selector: 'app-player[player], app-new-player',
-    templateUrl: './player.component.html',
-    styleUrls: ['./player.component.scss'],
-    changeDetection: ChangeDetectionStrategy.OnPush,
+  selector: 'app-player[player], app-new-player',
+  templateUrl: './player.component.html',
+  styleUrls: ['./player.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class PlayerComponent implements OnInit {
 
-    @ViewChild(NgForm) playerForm!: NgForm;
-    loading: boolean = false
-    playerState!: Player
-    newPlayer = false
+  @ViewChild(NgForm) playerForm!: NgForm;
+  loading: boolean = false
+  playerState!: Player
+  newPlayer = false
 
-    constructor(private playersService: PlayersService) {
+  constructor(private playersService: PlayersService) {
+  }
+
+  _player!: Player
+
+  @Input()
+  set player(player: Player) {
+    this.playerState = deepClone(player)
+    this._player = deepClone(this.playerState);
+    this.resetForm();
+  }
+
+  ngOnInit() {
+    if (!this._player) {
+      this.newPlayer = true;
+      this.player = {
+        id: -1,
+        firstName: "",
+        lastName: "",
+        email: ""
+      };
     }
+  }
 
-    _player!: Player
+  onSave() {
+    this.playerForm.control.markAllAsTouched();
 
-    @Input()
-    set player(player: Player) {
-        this.playerState = deepClone(player)
-        this._player = deepClone(this.playerState);
-        this.resetForm();
+    if (!this.playerForm.valid) return
+    this.loading = true;
+    if (this.newPlayer) {
+      this.playersService.addPlayer(this._player)
+        .subscribe(() => {
+          this.loading = false;
+          this.resetForm();
+        });
+    } else {
+      this.playersService.editPlayer(this._player)
+        .subscribe(() => this.loading = false);
     }
+  }
 
-    ngOnInit() {
-        if (!this._player) {
-            this.newPlayer = true;
-            this.player = {
-                id: -1,
-                firstName: "",
-                lastName: "",
-                email: ""
-            };
-        }
-    }
+  resetForm() {
+    if (!this.playerForm) return
+    this.playerForm.resetForm(deepClone(this.playerState))
+  }
 
-    onSave() {
-        this.playerForm.control.markAllAsTouched();
+  onDelete() {
+    this.loading = true;
+    this.playersService.deletePlayer(this._player.id)
+      .subscribe(() => this.loading = false);
+  }
 
-        if (!this.playerForm.valid) return
-        this.loading = true;
-        if (this.newPlayer) {
-            this.playersService.addPlayer(this._player)
-                .subscribe(() => {
-                    console.log('Added')
-                    this.loading = false;
-                    this.resetForm();
-                });
-        } else {
-            this.playersService.editPlayer(this._player)
-                .subscribe(() => this.loading = false);
-        }
-    }
+  onEscape(event: Event) {
+    const target = event.target as HTMLElement
 
-    resetForm() {
-        if (!this.playerForm) return
-        this.playerForm.resetForm(deepClone(this.playerState))
-    }
-
-    onDelete() {
-        this.loading = true;
-        this.playersService.deletePlayer(this._player.id)
-            .subscribe(() => this.loading = false);
-    }
+    target.blur()
+    this.resetForm();
+  }
 }
