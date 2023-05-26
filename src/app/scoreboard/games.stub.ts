6d3b30d5547b3aa280a7a1290c98ae51a2c4e918
Tobias Wagner <Tobawa2601@gmail.com>
2023-05-26 13:43:50 +0200

    Created a Games Stub for the development
    foosball-frontend-16 Add the service for managing games

---
+++ src/app/scoreboard/games.stub.ts
@@ -0,0 +1,33 @@
+import { Game } from "./game/game.model";
+import { playersStub } from "../players/player/player.stub";
+
+export const gamesStub = new Map<Game['id'], Game>([
+
+  [0, {
+    id: 0,
+    playDate: new Date(2023, 4, 20, 12, 30, 25),
+    yellowTeam: { attacker: playersStub.get(0)!, defender: playersStub.get(1)! },
+    blackTeam: { attacker: playersStub.get(2)!, defender: playersStub.get(3)! },
+    scores: { black: 0, yellow: 6 },
+    winner: 'yellow'
+  }],
+
+  [1, {
+    id: 1,
+    playDate: new Date(2023, 4, 26, 13, 45, 30),
+    yellowTeam: { attacker: playersStub.get(2)!, defender: playersStub.get(1)! },
+    blackTeam: { defender: playersStub.get(3)!, attacker: playersStub.get(0)! },
+    scores: { black: 4, yellow: 6 },
+    winner: 'yellow'
+  }],
+
+  [2, {
+    id: 2,
+    playDate: new Date(2023, 4, 26, 15, 36, 15),
+    yellowTeam: { attacker: playersStub.get(2)!, defender: playersStub.get(1)! },
+    blackTeam: { defender: playersStub.get(3)!, attacker: playersStub.get(0)! },
+    scores: { black: 10, yellow: 6 },
+    winner: 'black'
+  }],
+
+])
