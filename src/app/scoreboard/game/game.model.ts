9a5f3dd30e238a558ba87c8972fc8e220410689d
Tobias Wagner <Tobawa2601@gmail.com>
2023-05-26 13:29:40 +0200

    Created models for the representation of a game
    foosball-frontend-16 Add the service for managing games

--- src/app/scoreboard/game/game.model.ts
+++ src/app/scoreboard/game/game.model.ts
@@ -1,3 +1,15 @@
-export type Game = {}
+import { Team } from "./team.model";
+
+export type Game = {
+  readonly id: number,
+  playDate: Date,
+  yellowTeam: Team,
+  blackTeam: Team,
+  scores: {
+    yellow: number,
+    black: number
+  },
+  winner: 'yellow' | 'black' | 'draw'
+}
 
 export type Games = Game[]
---
+++ src/app/scoreboard/game/team.model.ts
@@ -0,0 +1,6 @@
+import { Player } from "../../players/player/player.model";
+
+export type Team = {
+  attacker: Player,
+  defender: Player
+}
