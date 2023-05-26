bf7b7ea06f59bc01110c0c8db66d554ba5860e51
Tobias Wagner <Tobawa2601@gmail.com>
2023-05-26 13:54:12 +0200

    Added the add, delete and edit functionality to the games.service.ts
    foosball-frontend-16 Add the service for managing games

--- src/app/scoreboard/games.service.ts
+++ src/app/scoreboard/games.service.ts
@@ -2,6 +2,7 @@ import { computed, Injectable, signal } from '@angular/core';
 import { Game, Games } from "./game/game.model";
 import { environment } from "../../environments/environment";
 import { gamesStub } from "./games.stub";
+import { delay, of, take, tap } from "rxjs";
 
 @Injectable({
   providedIn: 'root'
@@ -13,6 +14,51 @@ export class GamesService {
 
   constructor() {
     this.fetchGames()
+  }
+
+  public getGame(id: Game['id']) {
+    return this.gamesMap().get(id);
+  }
+
+  public addGame(game: Omit<Game, 'id'>) {
+    const id = (Array.from(this.gamesMap().keys()).sort().at(-1) ?? 0) + 1
+
+    return of({ ...game, id })
+      .pipe(
+        take(1),
+        delay(2000),
+        tap(addedGame => {
+          this.gamesMap.mutate(gamesMap => gamesMap.set(addedGame.id, addedGame));
+        })
+      )
+  }
+
+  public deleteGame(id: Game['id']) {
+    return of(id)
+      .pipe(
+        take(1),
+        delay(2000),
+        tap(deletedId => {
+          this.gamesMap.mutate(gamesMap => {
+            gamesMap.delete(deletedId)
+          })
+        })
+      )
+  }
+
+  public editGame(game: Game) {
+    return of(game)
+      .pipe(
+        take(1),
+        delay(2000),
+        tap(updatedGame => {
+          this.gamesMap.mutate(gamesMap => gamesMap.set(updatedGame.id, updatedGame));
+        })
+      )
+  }
+
+  public gameTrackBy(index: number, game: Game) {
+    return game.id
   }
 
   private fetchGames() {
