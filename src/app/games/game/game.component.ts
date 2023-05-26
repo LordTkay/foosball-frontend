473c372ce326506c0841d43ecfc80f53b56a4d15
Tobias Wagner <Tobawa2601@gmail.com>
2023-05-26 15:19:30 +0200

    Created a simple display of the games
    foosball-frontend-1 Creating a page that lists all played games

--- src/app/games/game/game.component.ts
+++ src/app/games/game/game.component.ts
@@ -1,5 +1,6 @@
 import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { Game } from "./game.model";
+import { TEAMS, Teams } from "./team.model";
 
 @Component({
   selector: 'app-game[game]',
@@ -10,5 +11,17 @@ import { Game } from "./game.model";
 export class GameComponent {
 
   @Input() game!: Game
+
+  getTeam(teamName: Teams | string | undefined | null) {
+    if (teamName == null || Object.values(TEAMS).findIndex(team => teamName) === -1) {
+      throw Error(`Team ${ teamName } does not exist!`)
+    }
+
+    return {
+      score: this.game.scores[teamName as Teams],
+      team: this.game.teams[teamName as Teams]!,
+      winner: this.game.winner === teamName
+    }
+  }
 
 }
