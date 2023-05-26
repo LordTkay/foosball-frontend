6baf4580a0b493e1acaca1d3cca0c44e636898b2
Tobias Wagner <Tobawa2601@gmail.com>
2023-05-26 13:45:12 +0200

    Created signal for the games and gamesmap and imported the games stub
    foosball-frontend-16 Add the service for managing games

--- src/app/scoreboard/games.service.ts
+++ src/app/scoreboard/games.service.ts
@@ -1,10 +1,25 @@
-import { Injectable } from '@angular/core';
+import { computed, Injectable, signal } from '@angular/core';
+import { Game, Games } from "./game/game.model";
+import { environment } from "../../environments/environment";
+import { gamesStub } from "./games.stub";
 
 @Injectable({
   providedIn: 'root'
 })
 export class GamesService {
 
+  private gamesMap = signal<Map<Game['id'], Game>>(new Map())
+  games = computed<Games>(() => Array.from(this.gamesMap().values()));
+
   constructor() {
+    this.fetchGames()
+  }
+
+  private fetchGames() {
+    if (environment.production) {
+      //ToDo Fetch Games from Backend
+    } else {
+      this.gamesMap.set(gamesStub)
+    }
   }
 }
