277db9473595ebe3566feb7629d778204e503227
Tobias Wagner <Tobawa2601@gmail.com>
2023-05-27 16:40:09 +0200

    Removed an unused function
    foosball-frontend-2 Add a game

--- src/app/games/game/game.component.ts
+++ src/app/games/game/game.component.ts
@@ -1,6 +1,5 @@
 import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
-import { Game } from "./game.model";
-import { TEAMS, Teams } from "./team.model";
+import { Game } from './game.model';
 
 @Component({
   selector: 'app-game[game]',
@@ -11,17 +10,5 @@ import { TEAMS, Teams } from "./team.model";
 export class GameComponent {
 
   @Input() game!: Game
-
-  getTeam(teamName: Teams | string | undefined | null) {
-    if (teamName == null || Object.values(TEAMS).findIndex(team => teamName) === -1) {
-      throw Error(`Team ${ teamName } does not exist!`)
-    }
-
-    return {
-      score: this.game.scores[teamName as Teams],
-      team: this.game.teams[teamName as Teams]!,
-      winner: this.game.winner
-    }
-  }
 
 }
