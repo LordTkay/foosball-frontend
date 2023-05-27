c51205a6b85bd6d764c6b69eebc5954942837c8f
Tobias Wagner <Tobawa2601@gmail.com>
2023-05-27 14:52:44 +0200

    SortPipe modified to support multiple fields/rules at the same time foosball-frontend-2 Add a game

--- src/app/pipes/sort-by-date.pipe.ts
+++ src/app/pipes/sort-by-date.pipe.ts
@@ -5,17 +5,26 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class SortByPipe implements PipeTransform {
 
-  transform<T extends unknown>(array: T[], attributName: keyof T): T[] {
+  transform<T extends unknown>(array: T[], attributNames: keyof T | (keyof T)[]): T[] {
     return array.sort((a, b) => {
-      const valueA = a[attributName];
-      const valueB = b[attributName];
+      const fields = Array.isArray(attributNames) ? attributNames : [attributNames];
 
-      if (valueA instanceof Date && valueB instanceof Date) {
-        return valueA.getTime() > valueB.getTime() ? -1 : 0;
-      }
+      for (let field of fields) {
+        const valueA = a[field];
+        const valueB = b[field];
 
-      if (typeof valueA === 'string' && typeof valueB === 'string') {
-        return valueA.localeCompare(valueB);
+        if (valueA instanceof Date && valueB instanceof Date) {
+          if (valueA.getTime() > valueB.getTime()) return -1;
+          if (valueA.getTime() < valueB.getTime()) return 1;
+
+        } else if (typeof valueA === 'string' && typeof valueB === 'string') {
+          const compare = valueA.localeCompare(valueB);
+          if (compare !== 0) return compare;
+
+        } else if (typeof valueA === 'number' && typeof valueB === 'number') {
+          if (valueA > valueB) return -1;
+          if (valueA < valueB) return 1;
+        }
       }
 
       throw new Error('SortByDate-Pipe can only sort by Date or String. Others types need to be added, if needed!');
