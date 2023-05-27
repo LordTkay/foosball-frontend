58e59caa770f90179244521553a6ebecdde03264
Tobias Wagner <Tobawa2601@gmail.com>
2023-05-27 12:54:37 +0200

    Removed an unnecessary and unused type
    foosball-frontend-2 Add a game

--- src/app/types/form-group.type.ts
+++
@@ -1,8 +0,0 @@
-import { FormControl } from "@angular/forms";
-
-/**
- * Maps all attribute types of an object to a Angular FormControl type.
- */
-export type ObjectToFormGroup<T extends object> = {
-    [K in keyof T]-?: FormControl<T[K]>
-}
