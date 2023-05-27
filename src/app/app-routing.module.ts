22a40189cdd709fe5614b5ec29d18b76a553e45a
Tobias Wagner <Tobawa2601@gmail.com>
2023-05-27 11:22:33 +0200

    Added new child routes to the games route, that lets you create a new entry or edit existing ones
    foosball-frontend-2 Add a game

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { PlayersComponent } from "./players/players.component";
-import { ScoreboardComponent } from "./scoreboard/scoreboard.component";
-import { GamesComponent } from "./games/games.component";
+import { PlayersComponent } from './players/players.component';
+import { ScoreboardComponent } from './scoreboard/scoreboard.component';
+import { GamesComponent } from './games/games.component';
+import { GameEditComponent } from './games/game-edit/game-edit.component';
+import { gameGuard } from './games/game.guard';
 
 const routes: Routes = [
   { path: 'players', component: PlayersComponent },
   { path: 'scoreboard', component: ScoreboardComponent },
-  { path: 'games', component: GamesComponent },
+  {
+    path: 'games',
+    children: [
+      { path: '', component: GamesComponent, pathMatch: 'full' },
+      { path: ':id/edit', component: GameEditComponent, canActivate: [gameGuard] },
+      { path: 'new', component: GameEditComponent, data: { newEntry: true } }
+    ]
+  },
 ];
 
 @NgModule({
