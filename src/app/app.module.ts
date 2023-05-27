d1323916322f2113551c824cda125728c8fb0c37
Tobias Wagner <Tobawa2601@gmail.com>
2023-05-27 11:05:22 +0200

    Create a new component for adding and editing games
    foosball-frontend-2 Add a game

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,24 @@
-import { DEFAULT_CURRENCY_CODE, LOCALE_ID, NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {DEFAULT_CURRENCY_CODE, LOCALE_ID, NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HttpClientModule } from "@angular/common/http";
-import { registerLocaleData } from "@angular/common";
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {HttpClientModule} from "@angular/common/http";
+import {registerLocaleData} from "@angular/common";
 import localeDe from '@angular/common/locales/de';
-import { HeaderComponent } from './components/header/header.component';
-import { NavigationComponent } from './components/navigation/navigation.component';
-import { PlayersComponent } from './players/players.component';
-import { ScoreboardComponent } from './scoreboard/scoreboard.component';
-import { PlayerComponent } from './players/player/player.component';
-import { FormsModule } from "@angular/forms";
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { LoadingComponent } from './components/loading-spinner/loading.component';
-import { GameComponent } from './games/game/game.component';
-import { GamesComponent } from './games/games.component';
-import { TeamOverviewComponent } from './games/game/team-overview/team-overview.component';
-import { SortByPipe } from "./pipes/sort-by-date.pipe";
+import {HeaderComponent} from './components/header/header.component';
+import {NavigationComponent} from './components/navigation/navigation.component';
+import {PlayersComponent} from './players/players.component';
+import {ScoreboardComponent} from './scoreboard/scoreboard.component';
+import {PlayerComponent} from './players/player/player.component';
+import {FormsModule} from "@angular/forms";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {LoadingComponent} from './components/loading-spinner/loading.component';
+import {GameComponent} from './games/game/game.component';
+import {GamesComponent} from './games/games.component';
+import {TeamOverviewComponent} from './games/game/team-overview/team-overview.component';
+import {SortByPipe} from "./pipes/sort-by-date.pipe";
+import {GameEditComponent} from './games/game-edit/game-edit.component';
 
 registerLocaleData(localeDe)
 
@@ -36,6 +37,7 @@ registerLocaleData(localeDe)
     TeamOverviewComponent,
 
     SortByPipe,
+    GameEditComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/games/game-edit/game-edit.component.ts
@@ -0,0 +1,11 @@
+import {ChangeDetectionStrategy, Component} from '@angular/core';
+
+@Component({
+  selector: 'app-game-edit',
+  templateUrl: './game-edit.component.html',
+  styleUrls: ['./game-edit.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class GameEditComponent {
+
+}
