f8ee2bedd1afd8645f77bd1641504a71c5a6db10
Tobias Wagner <Tobawa2601@gmail.com>
2023-05-27 14:57:01 +0200

    SortPipe modified that you can either use ascending or descending order
    foosball-frontend-2 Add a game

--- src/app/pipes/sort-by-date.pipe.ts
+++ src/app/pipes/sort-by-date.pipe.ts
@@ -5,25 +5,27 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class SortByPipe implements PipeTransform {
 
-  transform<T extends unknown>(array: T[], attributNames: keyof T | (keyof T)[]): T[] {
+  transform<T extends unknown>(array: T[], attributNames: keyof T | (keyof T)[], direction: 'asc' | 'desc' = 'asc'): T[] {
     return array.sort((a, b) => {
       const fields = Array.isArray(attributNames) ? attributNames : [attributNames];
+
+      const directionFactor = direction === 'asc' ? 1 : -1;
 
       for (let field of fields) {
         const valueA = a[field];
         const valueB = b[field];
 
         if (valueA instanceof Date && valueB instanceof Date) {
-          if (valueA.getTime() > valueB.getTime()) return -1;
-          if (valueA.getTime() < valueB.getTime()) return 1;
+          if (valueA.getTime() > valueB.getTime()) return directionFactor;
+          if (valueA.getTime() < valueB.getTime()) return -directionFactor;
 
         } else if (typeof valueA === 'string' && typeof valueB === 'string') {
           const compare = valueA.localeCompare(valueB);
-          if (compare !== 0) return compare;
+          if (compare !== 0) return compare * directionFactor;
 
         } else if (typeof valueA === 'number' && typeof valueB === 'number') {
-          if (valueA > valueB) return -1;
-          if (valueA < valueB) return 1;
+          if (valueA > valueB) return -directionFactor;
+          if (valueA < valueB) return directionFactor;
         }
       }
 
