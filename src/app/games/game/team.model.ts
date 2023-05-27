d46883de8936dcf1add9668e272176fd37594239
Tobias Wagner <Tobawa2601@gmail.com>
2023-05-27 12:55:01 +0200

    Extracted the TeamPosition in their own type
    foosball-frontend-2 Add a game

--- src/app/games/game/team.model.ts
+++ src/app/games/game/team.model.ts
@@ -1,12 +1,11 @@
-import { Player } from "../../players/player/player.model";
+import { Player } from '../../players/player/player.model';
 
 export const TEAMS = {
   YELLOW: 'yellow',
   BLACK: 'black'
-} as const
+} as const;
 export type Teams = typeof TEAMS[keyof typeof TEAMS]
 
-export type Team = {
-  attacker: Player,
-  defender: Player
-}
+export type TeamPosition = 'attacker' | 'defender'
+
+export type Team = Record<TeamPosition, Player>
