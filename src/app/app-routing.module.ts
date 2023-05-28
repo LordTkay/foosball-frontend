eef65bfcc1bee787049d3afc6487ddbd427f18eb
Tobias Wagner <Tobawa2601@gmail.com>
2023-05-28 16:51:13 +0200

    Adding the resolvers to the urls that always need players and games

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,20 +1,29 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { PlayersComponent } from './players/players.component';
-import { ScoreboardComponent } from './scoreboard/scoreboard.component';
-import { GamesComponent } from './games/games.component';
-import { GameEditComponent } from './games/game-edit/game-edit.component';
-import { gameGuard } from './games/game.guard';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {PlayersComponent} from './players/players.component';
+import {ScoreboardComponent} from './scoreboard/scoreboard.component';
+import {GamesComponent} from './games/games.component';
+import {GameEditComponent} from './games/game-edit/game-edit.component';
+import {gameGuard} from './games/game.guard';
+import {gamesResolver} from "./games/games.resolver";
+import {playersResolver} from "./players/playersResolver";
 
 const routes: Routes = [
-  { path: 'players', component: PlayersComponent },
-  { path: 'scoreboard', component: ScoreboardComponent },
   {
-    path: 'games',
+    path: '',
+    resolve: [playersResolver, gamesResolver],
     children: [
-      { path: '', component: GamesComponent, pathMatch: 'full' },
-      { path: ':id/edit', component: GameEditComponent, canActivate: [gameGuard] },
-      { path: 'new', component: GameEditComponent, data: { newEntry: true } }
+      {path: '', redirectTo: 'games', pathMatch: 'full'},
+      {path: 'players', component: PlayersComponent},
+      {path: 'scoreboard', component: ScoreboardComponent},
+      {
+        path: 'games',
+        children: [
+          {path: '', component: GamesComponent, pathMatch: 'full'},
+          {path: ':id/edit', component: GameEditComponent, canActivate: [gameGuard]},
+          {path: 'new', component: GameEditComponent, data: {newEntry: true}}
+        ]
+      },
     ]
   },
 ];
