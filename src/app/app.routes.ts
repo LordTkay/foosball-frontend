8e9b7938b299560c218f47069c40893e1f0ca3da
Tobias Wagner <tobawa2601@gmail.com>
2024-12-15 11:35:19 +0100

    Created new components for games and players. Also added their routes.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'games', pathMatch: 'full' },
+    { path: 'games', loadComponent: () => import('./games').then(m => m.GamesComponent) },
+    { path: 'players', loadComponent: () => import('./players').then(m => m.PlayersComponent) }
+];
